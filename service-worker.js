02ac0c0ca20a1728dde56eb2847b2e665a16db21
waiwai <147108916+Yuge-U@users.noreply.github.com>
2026-07-26 08:05:18 +0900

    連続再生の動画保存機能を追加 (#12)
    コートCanvasから全STEPを録画し、対応形式の動画ファイルとして保存できるようにします。

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // GitHub Pages上でアプリ本体をオフライン利用できるようにします。
-const CACHE_NAME = "basketball-tactics-board-v32-court-copyright";
+const CACHE_NAME = "basketball-tactics-board-v33-video-export";
 
 // アプリ本体として保存するファイルです。
 const APP_FILES = [
